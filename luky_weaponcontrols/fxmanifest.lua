0002386f52b40cb3ff537f007c5cc98d460fcf2b
LukyNo <52509533+LukyNo1@users.noreply.github.com>
2023-04-22 14:17:55 +0200

    The base of the script

---
+++ luky_weaponcontrols/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'LukyNo'
+description 'Basic script to toggle weapon-controlls by command!'
+version '1.0'
+lua54 'yes'
+
+client_scripts {
+    'client.lua',
+    'config.lua'
+}
+
+shared_script 'config.lua'
