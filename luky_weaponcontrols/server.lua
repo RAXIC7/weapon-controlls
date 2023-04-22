53ec641dbdd5ba08d3f44a8188e34d3ca92387ec
LukyNo <52509533+LukyNo1@users.noreply.github.com>
2023-04-22 14:22:54 +0200

    Version Check
    Created server.lua for version check

---
+++ luky_weaponcontrols/server.lua
@@ -0,0 +1,26 @@
+local scriptVersion = "1.0"
+local githubLink = "https://github.com/mojrepository/moj-skript"
+
+function checkVersion()
+    PerformHttpRequest(githubLink .. "/releases/latest", function(err, text, headers)
+        if err == 200 then
+            local latestVersion = string.match(text, 'tag/v(%d+.%d+)')
+            if latestVersion and latestVersion ~= scriptVersion then
+                print("^1New Version of the script is released " .. githubLink)
+            end
+        end
+    end, "GET", "", {["Content-Type"] = "application/json"})
+end
+
+Citizen.CreateThread(function()
+    checkVersion()
+end)
+
+Citizen.CreateThread(function()
+    if GetCurrentResourceName() ~= "luky_weaponcontrols" then
+      print("-----------------------------------------------------")
+      print("Please Dont Change the resource name to avoid bugs and errors.")
+	    print("Script made by LukyNo")
+      print("-----------------------------------------------------")
+    end
+end)
